a24c56e4541bd393d8ac21d60df2eeb1e1dd4936
Guilherme Mendonça <guilhermemg_93@hotmail.com>
2022-04-11 22:02:54 -0300

    Basic struture setup

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const imgRef = document.getElementById('target.image')
+const imgRef = document.getElementById('target-image');
+const lodingTextRef = document.getElementById('loading-text');
+const buttonRef = document.getElementById('button-load');
+
+let loading = false;
 
 const url = 'https://dog.ceo/api/breeds/image/random';
 
@@ -8,15 +12,40 @@ const options = {
 
 const handleResponse = async (response) => {
   const data = await response.json();
-  console.log(data);
   const { message } = data;
   imgRef.src = message;
+  imgRef.onload = () => {
+    loading = false;
+    lodingTextRef.style = "opacity: 0";
+    imgRef.style = "opacity: 1";
+    buttonRef.disabled = false;
+  }
 }
 
 const handleError = () => {
+  loading = false;
+  lodingTextRef.style = "opacity: 0";
+  imgRef.style = "opacity: 1";
+  buttonRef.disabled = false;
   console.log('ERROR');
 }
 
-const request = fetch(url, options);
-request.then(handleResponse);
-request.catch(handleError);
+const requestDog = () => {
+  loading = true;
+  imgRef.style = "opacity: 0";
+  lodingTextRef.style = "opacity: 1";
+  buttonRef.disabled = true;
+
+  const request = fetch(url, options);
+  request.then(handleResponse);
+  request.catch(handleError);
+}
+
+const handleButtonClick = () => {
+  if (loading) {
+    return;
+  }
+  requestDog();
+}
+
+requestDog();
